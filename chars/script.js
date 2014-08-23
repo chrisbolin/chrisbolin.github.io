84be7ee9f068e462fb2a4c5f2b3230142bd6dc08
Chris Bolin <bolin.chris@gmail.com>
2014-08-23 10:26:49 -0400

    basic framework (and demo) for chars site

---
+++ chars/script.js
@@ -0,0 +1,20 @@
+var app = angular.module('charsApp', []);
+
+var symbols = [
+	{codes:['&circledast;'],tags:['circle','asterisk','operator']},
+	{codes:['&CircleDot;'],tags:['circle','dot','operator']},
+	{codes:['&osol;'],tags:['circle','division','operator']},
+	{codes:['&CircleTimes;'],tags:['circle','times','operator']},
+	{codes:['&CirclePlus;'],tags:['circle','plus','operator']},
+	{codes:['&CircleMinus;'],tags:['circle','Minus','operator']}
+];
+
+app.controller('MainController', function($scope,$sce) {
+	$scope.symbols = symbols;
+	for (var i in $scope.symbols) {
+		var symbol = $scope.symbols[i];
+		symbol.printable = $sce.trustAsHtml(symbol.codes[0])
+	};
+	$sce.trustAsHtml()
+	$scope.symbols = symbols;
+});
