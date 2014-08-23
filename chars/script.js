493b13e3d053dd8bfd07654f3686e1a8900a0058
Chris Bolin <bolin.chris@gmail.com>
2014-08-23 18:48:07 -0400

    styled, semi-responsive, searching.

--- chars/script.js
+++ chars/script.js
@@ -6,11 +6,19 @@ var symbols = [
 	{codes:['&osol;'],tags:['circle','division','operator']},
 	{codes:['&CircleTimes;'],tags:['circle','times','operator']},
 	{codes:['&CirclePlus;'],tags:['circle','plus','operator']},
-	{codes:['&CircleMinus;'],tags:['circle','Minus','operator']}
+	{codes:['&CircleMinus;'],tags:['circle','Minus','operator']},
+	// ------------------------------------ //
+	{codes:['&CirclePlus;'],tags:['circle','plus','operator']},
+	{codes:['&CircleMinus;'],tags:['circle','Minus','operator']},
+	{codes:['&CircleDot;'],tags:['circle','dot','operator']},
+	{codes:['&circledast;'],tags:['circle','asterisk','operator']},
+	{codes:['&CircleTimes;'],tags:['circle','times','operator']},
+	{codes:['&osol;'],tags:['circle','division','operator']}
 ];
 
 app.controller('MainController', function($scope,$sce) {
 	$scope.symbols = symbols;
+
 	for (var i in $scope.symbols) {
 		var symbol = $scope.symbols[i];
 		symbol.printable = $sce.trustAsHtml(symbol.codes[0])
