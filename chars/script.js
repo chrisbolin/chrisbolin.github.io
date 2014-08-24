364a8faa5e10ddac136fd6092a17a8a1a9ca21a1
Chris Bolin <bolin.chris@gmail.com>
2014-08-23 20:10:18 -0400

    adding all chars from w3

--- chars/script.js
+++ chars/script.js
@@ -1,21 +1,5 @@
 var app = angular.module('charsApp', []);
 
-var symbols = [
-	{codes:['&circledast;'],tags:['circle','asterisk','operator']},
-	{codes:['&CircleDot;'],tags:['circle','dot','operator']},
-	{codes:['&osol;'],tags:['circle','division','operator']},
-	{codes:['&CircleTimes;'],tags:['circle','times','operator']},
-	{codes:['&CirclePlus;'],tags:['circle','plus','operator']},
-	{codes:['&CircleMinus;'],tags:['circle','Minus','operator']},
-	// ------------------------------------ //
-	{codes:['&CirclePlus;'],tags:['circle','plus','operator']},
-	{codes:['&CircleMinus;'],tags:['circle','Minus','operator']},
-	{codes:['&CircleDot;'],tags:['circle','dot','operator']},
-	{codes:['&circledast;'],tags:['circle','asterisk','operator']},
-	{codes:['&CircleTimes;'],tags:['circle','times','operator']},
-	{codes:['&osol;'],tags:['circle','division','operator']}
-];
-
 app.controller('MainController', function($scope,$sce) {
 	$scope.symbols = symbols;
 
