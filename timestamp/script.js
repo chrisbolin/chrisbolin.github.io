e4d4ea5c6d7f3ce6beb6be77df30c135840b8c50
Chris Bolin <bolin.chris@gmail.com>
2014-09-17 00:25:21 -0400

    more info on click and hover

--- timestamp/script.js
+++ timestamp/script.js
@@ -1,5 +1,18 @@
 var app = angular.module('timestampApp', []);
 
+details = [
+	'~32 years',
+	'~3 years',
+	'~4 months',
+	'~11 days',
+	'~1 day',
+	'~3 hours',
+	'~15 minutes',
+	'~1.5 minutes',
+	'10 seconds',
+	'1 second',
+]
+
 app.controller('MainCtrl', function($scope) {
 	$scope.timestamp = 'init';
 	updateTimestamp = function(){
@@ -9,4 +22,8 @@ app.controller('MainCtrl', function($scope) {
 	}
 	var delay = 250;
 	var intervalID = window.setInterval(updateTimestamp, delay);
+	$scope.getInfo = function(index){
+		console.log(index);
+		$scope.info = details[index];
+	}
 });
