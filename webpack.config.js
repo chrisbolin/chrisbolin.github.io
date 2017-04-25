2791c9e735f5218532338770463e18ba44595abb
Chris Bolin <bolin.chris@gmail.com>
2017-04-25 23:00:42 +0300

    prod and dev webpack configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,37 @@
 const { resolve } = require('path');
 const webpack = require('webpack');
 
-module.exports = {
+const baseExports = {
   context: resolve(__dirname, 'src'),
 
+  entry: './index.js',
+
+  output: {
+    filename: 'bundle.js', // the output bundle
+    path: __dirname,
+    publicPath: '/' // necessary for HMR to know where to load the hot update chunks
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [ 'babel-loader' ],
+        exclude: /node_modules/
+      },
+    ],
+  },
+};
+
+const devExports = {
   entry: [
     'react-hot-loader/patch', // activate HMR for React
     'webpack-dev-server/client?http://localhost:8080',
     // bundle client for webpack-dev-server and connect to the provided endpoint
     'webpack/hot/only-dev-server',
     // only hot reload for successful updates
-    './index.js' // the entry point of our app
+    './index.js'
   ],
-  output: {
-    filename: 'bundle.js', // the output bundle
-    path: __dirname,
-    publicPath: '/' // necessary for HMR to know where to load the hot update chunks
-  },
 
   devtool: 'inline-source-map',
 
@@ -26,18 +41,13 @@ module.exports = {
     publicPath: '/' // match the output `publicPath`
   },
 
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        use: [ 'babel-loader', ],
-        exclude: /node_modules/
-      },
-    ],
-  },
-
   plugins: [
     new webpack.HotModuleReplacementPlugin(), // enable HMR globally
     new webpack.NamedModulesPlugin(), // prints more readable module names in browser console
   ],
+};
+
+module.exports = function (env) {
+  const dev = env !== 'prod'; // use dev mode unless explict
+  return Object.assign(baseExports, (dev ? devExports : {}));
 };
