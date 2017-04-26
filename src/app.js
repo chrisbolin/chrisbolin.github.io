e3fb7371860bd78330f20b450757d5a65d2b405a
Chris Bolin <bolin.chris@gmail.com>
2017-04-26 20:15:15 +0300

    move to const and stateless components

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-let fx = {
+const fx = {
   limitUnit(x) {
     return (x < 0) ? 0 : (
       (x < 1) ? x : 1
@@ -18,77 +18,69 @@ let fx = {
   },
 };
 
-let ColorBar = React.createClass({
-  render() {
-    const height = fx.limitUnit(this.props.x) * 100;
-    return (
-      <div style={{
-        width: this.props.width,
-        height: height + '%',
-        backgroundColor: this.props.color,
-        left: this.props.left,
-        position: 'absolute',
-        bottom: 0,
-      }}/>
-    );
-  }
-});
+const ColorBar = ({x, width, left, color}) => {
+  const height = fx.limitUnit(x) * 100;
+  return (
+    <div style={{
+      width,
+      height: height + '%',
+      backgroundColor: color,
+      left,
+      position: 'absolute',
+      bottom: 0,
+    }}/>
+  );
+}
 
-let CardFront = React.createClass({
-  render() {
-    const x = this.props.x;
-    const zIndex = (x < 0.5) ? 1 : 0;
-    if (!zIndex) return null;
-    const scrollStyle = {
-      opacity: (1 - 10 * x),
-    };
-    return (
-      <div className='card-face front' style={{zIndex}}>
-        <div className='title'>
-          chris bolin
-          <hr/>
-        </div>
-        <div className="scroll" style={scrollStyle}>
-          (scroll)
-        </div>
-        <ColorBar color="#EDC919" left="0%" width="20%" x={x*5.2}/>
-        <ColorBar color="#76919A" left="20%" width="20%" x={x*4.7}/>
-        <ColorBar color="#257A97" left="40%" width="20%" x={x*4.4}/>
-        <ColorBar color="#7A486E" left="60%" width="20%" x={x*4}/>
-        <ColorBar color="#FD556F" left="80%" width="20%" x={x*3.5}/>
+const CardFront = ({x}) => {
+  const zIndex = (x < 0.5) ? 1 : 0;
+  if (!zIndex) return null;
+  const scrollStyle = {
+    opacity: (1 - 10 * x),
+  };
+  return (
+    <div className='card-face front' style={{zIndex}}>
+      <div className='title'>
+        chris bolin
+        <hr/>
       </div>
-    );
-  }
-});
-
-let CardBack = React.createClass({
-  render() {
-    const x = this.props.x;
-    const zIndex = (x > 0.5) ? 1 : 0;
-    const style = {
-      zIndex,
-      backgroundColor: '#240f1f',
-    }
-
-    if (!zIndex) return null;
-    return (
-      <div className='card-face back' style={style}>
-        <ColorBar color="#EDC919" width="10%" left="0%" x={2*(1-x)}/>
-        <ColorBar color="#76919A" width="10%" left="10%" x={2.1*(1-x)}/>
-        <ColorBar color="#257A97" width="10%" left="20%" x={2.2*(1-x)}/>
-        <ColorBar color="#7A486E" width="10%" left="30%" x={2.3*(1-x)}/>
-        <ColorBar color="#EDC919" width="10%" left="40%" x={2.4*(1-x)}/>
-        <ColorBar color="#76919A" width="10%" left="50%" x={2.5*(1-x)}/>
-        <ColorBar color="#257A97" width="10%" left="60%" x={2.6*(1-x)}/>
-        <ColorBar color="#7A486E" width="10%" left="70%" x={2.7*(1-x)}/>
-        <ColorBar color="#EDC919" width="10%" left="80%" x={2.8*(1-x)}/>
-        <ColorBar color="#76919A" width="10%" left="90%" x={2.9*(1-x)}/>
+      <div className="scroll" style={scrollStyle}>
+        (scroll)
       </div>
-    );
-  }
-});
+      <ColorBar color="#EDC919" left="0%" width="20%" x={x*5.2}/>
+      <ColorBar color="#76919A" left="20%" width="20%" x={x*4.7}/>
+      <ColorBar color="#257A97" left="40%" width="20%" x={x*4.4}/>
+      <ColorBar color="#7A486E" left="60%" width="20%" x={x*4}/>
+      <ColorBar color="#FD556F" left="80%" width="20%" x={x*3.5}/>
+    </div>
+  );
+}
 
-let CardPlane = React.createClass({
+const CardBack = ({x}) => {
+  const zIndex = (x > 0.5) ? 1 : 0;
+  const style = {
+    zIndex,
+    backgroundColor: '#240f1f',
+  }
+
+  if (!zIndex) return <div/>;
+  return (
+    <div className='card-face back' style={style}>
+      <ColorBar color="#EDC919" width="10%" left="0%" x={2*(1-x)}/>
+      <ColorBar color="#76919A" width="10%" left="10%" x={2.1*(1-x)}/>
+      <ColorBar color="#257A97" width="10%" left="20%" x={2.2*(1-x)}/>
+      <ColorBar color="#7A486E" width="10%" left="30%" x={2.3*(1-x)}/>
+      <ColorBar color="#EDC919" width="10%" left="40%" x={2.4*(1-x)}/>
+      <ColorBar color="#76919A" width="10%" left="50%" x={2.5*(1-x)}/>
+      <ColorBar color="#257A97" width="10%" left="60%" x={2.6*(1-x)}/>
+      <ColorBar color="#7A486E" width="10%" left="70%" x={2.7*(1-x)}/>
+      <ColorBar color="#EDC919" width="10%" left="80%" x={2.8*(1-x)}/>
+      <ColorBar color="#76919A" width="10%" left="90%" x={2.9*(1-x)}/>
+    </div>
+  );
+}
+
+class CardPlane extends React.Component {
   getStyle() {
     const x = this.props.x;
     const rotateX = 180  * (
@@ -105,35 +97,32 @@ let CardPlane = React.createClass({
       transform,
       WebkitTransform: transform,
     };
-  },
+  }
   render() {
-    let zFront = this.props.x < 0.5 ? 1 : 0;
-    let zBack = !zFront;
+    const zFront = this.props.x < 0.5 ? 1 : 0;
+    const zBack = !zFront;
     return (
       <div style={this.getStyle()} className='card-plane'>
         <CardFront x={this.props.x}/>
         <CardBack x={this.props.x}/>
       </div>
     )
-  },
-});
+  }
+}
 
-let Arrow = React.createClass({
-  render() {
-    const x = this.props.x;
-    const grey = Math.floor(255 * (1 - x));
-    const transform = `translateY(${ 20 * x }px)`;
-    const style = {
-      transform,
-      WebkitTransform: transform,
-      color: `rgb(${grey},${grey},${grey})`,
-      opacity: fx.limitUnit(10 * (0.9 - x)),
-    };
-    return (
-      <div className="arrow" style={style}>&darr;</div>
-    );
-  },
-});
+const Arrow = ({x}) => {
+  const grey = Math.floor(255 * (1 - x));
+  const transform = `translateY(${ 20 * x }px)`;
+  const style = {
+    transform,
+    WebkitTransform: transform,
+    color: `rgb(${grey},${grey},${grey})`,
+    opacity: fx.limitUnit(10 * (0.9 - x)),
+  };
+  return (
+    <div className="arrow" style={style}>&darr;</div>
+  );
+};
 
 const Line = ({children, x, show}) => (
   <div
