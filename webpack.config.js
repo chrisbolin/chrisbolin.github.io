20c1308384a6e39d15a7c2b8f096098b0310276a
Chris Bolin <bolin.chris@gmail.com>
2019-08-02 15:37:38 -0600

    prettier webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,73 +1,73 @@
-const { resolve } = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
-const StyleExtHtmlWebpackPlugin = require('style-ext-html-webpack-plugin');
+const { resolve } = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ExtractTextWebpackPlugin = require("extract-text-webpack-plugin");
+const StyleExtHtmlWebpackPlugin = require("style-ext-html-webpack-plugin");
 
 const baseExports = {
-  context: resolve(__dirname, 'src'),
+  context: resolve(__dirname, "src"),
 
-  entry: './index.js',
+  entry: "./index.js",
 
   output: {
-    filename: 'bundle.js', // the output bundle
+    filename: "bundle.js", // the output bundle
     path: __dirname,
-    publicPath: '/' // necessary for HMR to know where to load the hot update chunks
+    publicPath: "/" // necessary for HMR to know where to load the hot update chunks
   },
 
   module: {
     rules: [
       {
         test: /\.js$/,
-        use: [ 'babel-loader' ],
+        use: ["babel-loader"],
         exclude: /node_modules/
       },
       {
         test: /\.css$/,
         loader: ExtractTextWebpackPlugin.extract({
-          use: 'css-loader',
-          fallback: 'style-loader',
+          use: "css-loader",
+          fallback: "style-loader"
         }),
         exclude: /node_modules/
-      },
-    ],
+      }
+    ]
   },
 
   plugins: [
     new HtmlWebpackPlugin({
-      template: resolve(__dirname, 'index.template.html'),
+      template: resolve(__dirname, "index.template.html")
     }),
-    new ExtractTextWebpackPlugin('styles.css'),
-    new StyleExtHtmlWebpackPlugin(),
-  ],
+    new ExtractTextWebpackPlugin("styles.css"),
+    new StyleExtHtmlWebpackPlugin()
+  ]
 };
 
 const devExports = {
   entry: [
-    'react-hot-loader/patch', // activate HMR for React
-    'webpack-dev-server/client?http://localhost:8080',
+    "react-hot-loader/patch", // activate HMR for React
+    "webpack-dev-server/client?http://localhost:8080",
     // bundle client for webpack-dev-server and connect to the provided endpoint
-    'webpack/hot/only-dev-server',
+    "webpack/hot/only-dev-server",
     // only hot reload for successful updates
-    './index.js'
+    "./index.js"
   ],
 
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
 
   devServer: {
     hot: true, // enable HMR on the server
     contentBase: __dirname, // match the output path
-    publicPath: '/' // match the output `publicPath`
+    publicPath: "/" // match the output `publicPath`
   },
 
   plugins: [
     ...baseExports.plugins,
     new webpack.HotModuleReplacementPlugin(), // enable HMR globally
-    new webpack.NamedModulesPlugin(), // prints more readable module names in browser console
-  ],
+    new webpack.NamedModulesPlugin() // prints more readable module names in browser console
+  ]
 };
 
-module.exports = function (env) {
-  const dev = env !== 'prod'; // use dev mode unless explict
-  return Object.assign(baseExports, (dev ? devExports : {}));
+module.exports = function(env) {
+  const dev = env !== "prod"; // use dev mode unless explict
+  return Object.assign(baseExports, dev ? devExports : {});
 };
