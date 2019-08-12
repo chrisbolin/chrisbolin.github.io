106c7b7fa8b10c5479ee6b954a0807a03ddfb576
Chris Bolin <bolin.chris@gmail.com>
2019-08-11 18:14:53 -0600

    minify html+css

--- scripts/markup.js
+++ scripts/markup.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 const React = require("react");
 const ReactDOMServer = require("react-dom/server");
+var htmlMinify = require("html-minifier").minify;
+
 const noop = () => {};
 spoofBrowser();
 
@@ -9,11 +11,19 @@ require("../bundle.js");
 const App = global.App;
 const renderedApp = ReactDOMServer.renderToString(React.createElement(App));
 
-const template = fs.readFileSync("index.html", "utf8");
-fs.writeFileSync(
-  "index.html",
-  template.replace("<!-- STATIC_RENDER -->", renderedApp)
+const markupTemplate = fs.readFileSync("index.html", "utf8");
+const markupRendered = markupTemplate.replace(
+  "<!-- STATIC_RENDER -->",
+  renderedApp
 );
+const markupMinified = htmlMinify(markupRendered, {
+  minifyCSS: true,
+  removeTagWhitespace: true,
+  collapseWhitespace: true,
+  collapseInlineTagWhitespace: true
+});
+
+fs.writeFileSync("index.html", markupMinified);
 
 function spoofBrowser() {
   window = {};
