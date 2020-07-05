1ad43c9947be935aca08b944a94da89328b63eb3
Chris Bolin <bolin.chris@gmail.com>
2020-07-05 11:05:11 -0600

    zoom to section

--- src/back.js
+++ src/back.js
@@ -1,48 +1,87 @@
 import { useState } from "react";
 
-function BackSection({ title, children }) {
+function BackSection({
+  title,
+  children,
+  onActivate,
+  onDeactivate,
+  active,
+  index,
+}) {
+  const transformOrigin = `${index < 2 ? "top" : "bottom"} ${
+    index % 2 === 0 ? "left" : "right"
+  }`;
   return (
-    <div className="BackSection">
-      <h1>
-        <button onClick={() => null}>{title}</button>
-      </h1>
-      {/* <div className="BackSection-children">{children}</div> */}
+    <div
+      style={{ transformOrigin }}
+      className={`BackSection ${active && "active"}`}
+      onClick={onActivate}
+    >
+      {active && (
+        <button className="BackSection-deactivate" onClick={onDeactivate}>
+          X
+        </button>
+      )}
+      {!active && (
+        <h1>
+          <button onClick={onActivate}>{title}</button>
+        </h1>
+      )}
+      {active && <div className="BackSection-children">{children}</div>}
     </div>
   );
 }
+
+const backSectionElements = [
+  <BackSection title="Chris Bolin">
+    Software engineer + artist in Denver. Engineering, Formidable. Founder, The
+    Disconnect
+  </BackSection>,
+  <BackSection title="Projects">
+    Offline Only, Tessellate, Skycoins, Elements, Enchiridion, Travels,
+    Shipwrecked
+  </BackSection>,
+  <BackSection title="Talks & Press">
+    SXSW me Convention, DinosaurJS, Debugging, Offline Websites, Vice,
+    Lifehacker, CBC, CJR, Le Monde, The Next Web
+  </BackSection>,
+  <BackSection title="CV">
+    VP Engineering, Formidable. Adjunct, U of Denver. Engineer, Jumpshell. Data
+    scientist, Autotegrity. Researcher, MIT EBM Lab. Engineer, NI Edu → Master’s
+    Computational Engineering, MIT, numerical simulation of environmental
+    impact. BS Mechanical Engineering, U of Nebraska
+  </BackSection>,
+];
 
 export default function Back({ x, style }) {
   // Text does not show until minX
   const minX = 0.6;
   const display = x > minX ? null : "none";
   const progress = (x - minX) / (1 - minX);
-  const [selectedSection, selectSection] = useState(null);
+  const [activeSectionIndex, setActiveSectionIndex] = useState(null);
+  const clearActiveSectionIndex = (e) => {
+    setActiveSectionIndex(null);
+    e.stopPropagation();
+  };
   const combinedStyle = {
     display,
     opacity: progress,
     filter: `brightness(${progress})`,
     ...style,
   };
+
   return (
     <div className="Back" style={combinedStyle}>
-      <BackSection title="Chris Bolin">
-        Software engineer + artist in Denver. Engineering, Formidable. Founder,
-        The Disconnect
-      </BackSection>
-      <BackSection title="Projects">
-        Offline Only, Tessellate, Skycoins, Elements, Enchiridion, Travels,
-        Shipwrecked
-      </BackSection>
-      <BackSection title="Talks & Press">
-        SXSW me Convention, DinosaurJS, Debugging, Offline Websites, Vice,
-        Lifehacker, CBC, CJR, Le Monde, The Next Web
-      </BackSection>
-      <BackSection title="CV">
-        VP Engineering, Formidable. Adjunct, U of Denver. Engineer, Jumpshell.
-        Data scientist, Autotegrity. Researcher, MIT EBM Lab. Engineer, NI Edu →
-        Master’s Computational Engineering, MIT, numerical simulation of
-        environmental impact. BS Mechanical Engineering, U of Nebraska
-      </BackSection>
+      {backSectionElements.map((element, index) =>
+        // index is a unique ID, as the number and order of this will not change at runtime
+        React.cloneElement(element, {
+          index,
+          key: index,
+          onActivate: () => setActiveSectionIndex(index),
+          onDeactivate: clearActiveSectionIndex,
+          active: index === activeSectionIndex,
+        })
+      )}
     </div>
   );
 }
