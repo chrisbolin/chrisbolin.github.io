e602caab9f5ef7fc5c2c9ecfd3af7ac67ff117ef
chrisbolin <chrisbolin@users.noreply.github.com>
2023-02-01 06:24:46 +0000

    deploy: fa67c583249007ae10f0c52a4df05b6d22aac523

--- _next/static/Bp0Vp2D5w4AP7nT80ylw5/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-e347dbbb123acc35ae47.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-d1045513926b2e647341.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
