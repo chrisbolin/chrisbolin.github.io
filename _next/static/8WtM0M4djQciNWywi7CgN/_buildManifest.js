dc5bcb83084efeb55749e86f741202af62f96322
chrisbolin <chrisbolin@users.noreply.github.com>
2025-12-16 19:22:18 +0000

    deploy: 79d25381c9bc443cf1178b2d6b7aa364cab476ed

---
+++ _next/static/8WtM0M4djQciNWywi7CgN/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-749ab43bc3fac96b.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
