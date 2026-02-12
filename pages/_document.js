8ee4c7cf79fe07a1f7ddebfb475ea2b3662de096
Chris Bolin <bolin.chris@gmail.com>
2026-02-11 21:26:38 -0700

    add xerox filter to back. bad perf on safari

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,25 @@ class CustomDocument extends Document {
       <Html lang="en">
         <Head />
         <body>
+          <svg width="0" height="0" style={{ position: "absolute" }}>
+            <defs>
+              <filter id="xerox">
+                <feTurbulence
+                  type="fractalNoise"
+                  baseFrequency="0.01 0.03"
+                  numOctaves="10"
+                  result="noise"
+                />
+                <feDisplacementMap
+                  in="SourceGraphic"
+                  in2="noise"
+                  scale="5"
+                  xChannelSelector="R"
+                  yChannelSelector="R"
+                />
+              </filter>
+            </defs>
+          </svg>
           <Main />
           <NextScript />
         </body>
